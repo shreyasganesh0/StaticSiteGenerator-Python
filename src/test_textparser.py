76e804285f518c158aca70d86ca033c7bf1bf4ba
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-11-14 13:25:03 -0500

    Add image and link regex matching

--- src/test_textparser.py
+++ src/test_textparser.py
@@ -109,6 +109,70 @@ class TestInlineMarkdown(unittest.TestCase):
                 ("another link", "https://blog.boot.dev"),
             ],
             matches,)
+    def test_split_image(self):
+        parser_obj = RawTextParser()
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.NORMAL,
+        )
+        new_nodes = parser_obj.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.NORMAL),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
 
+    def test_split_image_single(self):
+        parser_obj = RawTextParser()
+        node = TextNode(
+            "![image](https://www.example.COM/IMAGE.PNG)",
+            TextType.NORMAL,
+        )
+        new_nodes = parser_obj.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_images(self):
+        parser_obj = RawTextParser()
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.NORMAL,
+        )
+        new_nodes = parser_obj.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.NORMAL),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.NORMAL),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        parser_obj = RawTextParser()
+        node = TextNode(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+            TextType.NORMAL,
+        )
+        new_nodes = parser_obj.split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.NORMAL),
+                TextNode("another link", TextType.LINK, "https://blog.boot.dev"),
+                TextNode(" with text that follows", TextType.NORMAL),
+            ],
+            new_nodes,
+        )
 if __name__ == "__main__":
     unittest.main()
--- src/textparser.py
+++ src/textparser.py
@@ -29,6 +29,58 @@ class RawTextParser:
             new_nodes.extend(split_nodes)
         return new_nodes
 
+    def split_nodes_image(self, old_nodes):
+        new_nodes = []
+        for old_node in old_nodes:
+            if old_node.text_type != TextType.NORMAL:
+                new_nodes.append(old_node)
+                continue
+            original_text = old_node.text
+            images = self.extract_markdown_images(original_text)
+            if len(images) == 0:
+                new_nodes.append(old_node)
+                continue
+            for image in images:
+                sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+                if len(sections) != 2:
+                    raise ValueError("Invalid markdown, image section not closed")
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], TextType.NORMAL))
+                new_nodes.append(
+                    TextNode(
+                        image[0],
+                        TextType.IMAGE,
+                        image[1],
+                    )
+                )
+                original_text = sections[1]
+            if original_text != "":
+                new_nodes.append(TextNode(original_text, TextType.NORMAL))
+        return new_nodes
+
+
+    def split_nodes_link(self, old_nodes):
+        new_nodes = []
+        for old_node in old_nodes:
+            if old_node.text_type != TextType.NORMAL:
+                new_nodes.append(old_node)
+                continue
+            original_text = old_node.text
+            links = self.extract_markdown_links(original_text)
+            if len(links) == 0:
+                new_nodes.append(old_node)
+                continue
+            for link in links:
+                sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+                if len(sections) != 2:
+                    raise ValueError("Invalid markdown, link section not closed")
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], TextType.NORMAL))
+                new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+                original_text = sections[1]
+            if original_text != "":
+                new_nodes.append(TextNode(original_text, TextType.NORMAL))
+        return new_nodes
     def extract_markdown_images(self,text):
         pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
         matches = re.findall(pattern, text)
